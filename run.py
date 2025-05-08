84fe9b952e24a3c50f6805d7f037a53eec0812f9
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-05-08 16:09:45 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+from DataTransform import DataTransformer
+
+dt = DataTransformer(
+    dataset_path="ais.csv",
+    time_col="t",
+    id_col="shipid",
+    speed_col="speed",
+    heading_col="heading",
+    lat_col="lat",
+    long_col="lon",
+    course_col="course",
+    shiptype_col="shiptype",
+    destination_col="destination",
+    numeric_cols=['heading','course','speed'],
+    categorical_cols=['shiptype','destination']
+)
+
+dt.load_data()
+dt.transfrom_dataset()
+features_df = dt.get_all_features()
+print(features_df)
