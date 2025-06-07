88bb315174dffa69aa038a7efbf25aecf5391fdf
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-07 13:42:48 +0300

    Update time_utils.py
    fixed the line 8 instead of seconds it needed second

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -5,5 +5,5 @@ def categorize_time(df,time_col):
     if time_col not in df.columns:
         raise ValueError(f'{time_col} not in the dataset')
     df[time_col] = pd.to_datetime(df[time_col])
-    df.loc[(df[time_col].dt.minute==59) & (df[time_col].dt.seconds==59),time_col] = df[time_col] + pd.Timedelta(seconds=1)
+    df.loc[(df[time_col].dt.minute==59) & (df[time_col].dt.second==59),time_col] = df[time_col] + pd.Timedelta(seconds=1)
     return df
