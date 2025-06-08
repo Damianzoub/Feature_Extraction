187ab902567964844c1cbcd2e875be7e7bbfc293
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-08 15:08:52 +0300

    Update trajectory.py
    Add more Travelling Time and Location information Features in the script

--- features/trajectory.py
+++ features/trajectory.py
@@ -14,9 +14,23 @@ def trajectory(df,id_col,time_col,lat_col,long_col):
         start_time=(time_col, 'first'),
         end_time=(time_col, 'last')
     )
+    #Travelling Time and Location Information for every feature
     features['duration_second'] = (features['end_time']-features['start_time']).dt.total_seconds()
     features['duration_hour'] = features['duration_second']/3600
+    features['start_year'] = features['start_time'].dt.year
+    features['start_month'] = features['start_time'].dt.month
+    features['start_day'] = features['start_time'].dt.day
+    features['start_hour'] = features['start_time'].dt.hour
+    features['start_minute'] = features['start_time'].dt.minute
+    features['end_year'] = features['end_time'].dt.year
+    features['end_month'] = features['end_time'].dt.month
+    features['end_day'] = features['end_time'].dt.day
+    features['end_hour'] = features['end_time'].dt.hour
+    features['end_minute'] = features['end_time'].dt.minute
+    
     features = features.reset_index()
-    return features[[id_col,'start_lat','start_long','end_lat',
-                          'end_long','start_time','end_time','duration_second'
-                          ,'duration_hour']]
+    return features[[self.id_col, 'start_lat', 'start_lon', 'end_lat', 'end_lon',
+           'start_time', 'end_time', 'duration_second', 'duration_hour',
+           'start_year', 'start_month', 'start_day', 'start_hour', 'start_minute',
+           'end_year', 'end_month', 'end_day', 'end_hour', 'end_minute'
+           ]]
