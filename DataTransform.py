8c591d8401a1c0943387f9756fb2b9ed11c3b2f4
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-19 20:34:13 +0300

    Update DataTransform.py
    Implemented in the DataTransformer the FeaturePipeline to take all the values and return them

--- DataTransform.py
+++ DataTransform.py
@@ -2,17 +2,8 @@ import pandas as pd
 #file path utils for 
 from utils.data_loader import load_csv
 from utils.Imputer import transform_dataset
-from utils.time_utils import categorize_time
-from features.speed import average_speed_per_id
-from features.acceleration import acceleration_per_id
-from features.rot import rot_per_id
-from features.trajectory import trajectory
-from features.distance_and_straightness import _compute_total_and_straightness_metrics
-from features.max_spatial_spread import compute_max_spatial_spread
-from features.curvature import curvature_results
-from features.stops import count_stops
 from utils.cache_utils import save_cache,load_cache
-
+from feature_pipeline.feature_pipeline import FeaturePipeline
 class DataTransformer:
     def __init__(self,dataset_path,time_col='t',id_col='shipid',speed_col='speed',
                  heading_col='heading',lat_col='lat',lon_col='lon',course_col='course'
@@ -32,6 +23,19 @@ class DataTransformer:
         self.numeric_cols = numeric_cols
         self.categorical_cols=categorical_cols
 
+        self.col_kwargs = {
+            "dataset_path":dataset_path,
+            "time_col": time_col,
+            "id_col": id_col,
+            "speed_col": speed_col,
+            "heading_col": heading_col,
+            "lat_col": lat_col,
+            "lon_col": lon_col,
+            "course_col": course_col,
+            "shiptype_col": shiptype_col,
+            "destination_col": destination_col
+        }
+
     def load_data(self):
         self.data = load_csv(self.dataset_path)
     
@@ -43,76 +47,24 @@ class DataTransformer:
     def exist_null(self):
         return [(col,self.data[col].isnull().sum()) for col in self.data.columns if self.data[col].isnull().sum() >0 ] or None
 
-    #Returns a DataFrame with some statistical features for every ID
-    def statistical_measures(self):
-         speed = average_speed_per_id(self.data,self.id_col,self.time_col,self.speed_col)
-         acceleration = acceleration_per_id(self.data,self.time_col,self.id_col,self.speed_col)
-         rot = rot_per_id(self.data,self.heading_col,self.id_col,self.time_col)
-         curvature = curvature_results(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-
-         return (
-              speed.merge(acceleration,on=self.id_col)
-              .merge(rot,on=self.id_col)
-              .merge(curvature,on=self.id_col)
-         )
-    #Returns DataFrame with features per se
-    def features_per_se(self):
-         traj = trajectory(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-         distance_metrics = _compute_total_and_straightness_metrics(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-         max_spatial_spread = compute_max_spatial_spread(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-         stop = count_stops(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col,self.speed_col)
-         return (
-              traj.merge(distance_metrics,on=self.id_col)
-              .merge(max_spatial_spread,on=self.id_col)
-              .merge(stop,on=self.id_col)
-         )
+    
 
     def extract_features(self,mode='all'):
         """
         mode: 'all' | 'statistical' | 'per_se'
         Returns the selected feature set.
         """
+        feature_pipeline = FeaturePipeline(**self.col_kwargs)
         if mode == "statistical":
-            return self.statistical_measures()
+            return feature_pipeline.statistical_measures(self.data)
         elif mode == "per_se":
-            return self.features_per_se()
+            return feature_pipeline.features_per_se(self.data)
         elif mode == "all":
-            return self.get_all_features()
+            return feature_pipeline.extract_all(self.data)
         else:
             raise ValueError("Invalid mode. Choose from: 'all', 'statistical', or 'per_se'")
         
-    """
-    implemented to put and save the data to cached file
-    to save time with the calculations
-    """
-    def get_cached_features(self,mode='all',cache_path='cache/features_all.pkl'):
-        features = load_cache(cache_path)
-        if features is not None:
-            print("Loaded features from cache")
-            return features
-
-        print("Computing features....")
-        features = self.extract_features(mode=mode)
-        save_cache(features,cache_path)
-        print("Saved features to cache")
-        return features
     
-    def get_all_features(self):
-        speed = average_speed_per_id(self.data,self.id_col,self.time_col,self.speed_col)
-        acceleration= acceleration_per_id(self.data,self.time_col,self.id_col,self.speed_col)
-        rot = rot_per_id(self.data,self.heading_col,self.id_col,self.time_col)
-        traj = trajectory(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-        distance_metrics = _compute_total_and_straightness_metrics(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-        max_spatial_spread = compute_max_spatial_spread(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-        curvature = curvature_results(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col)
-        stop = count_stops(self.data,self.id_col,self.time_col,self.lat_col,self.lon_col,self.speed_col)
-
-        return (speed.merge(acceleration,on=self.id_col)
-                .merge(rot,on=self.id_col)
-                .merge(traj,on=self.id_col)
-                .merge(distance_metrics,on=self.id_col)
-                .merge(max_spatial_spread,on=self.id_col)
-                .merge(curvature,on=self.id_col)
-                .merge(stop,on=self.id_col)
-                )
+    
+    
         
