d627d3f455ac6066c329e99845c4b76cae154b23
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-19 20:11:29 +0300

    Update cache_utils.py
    upgraded the cache_utils to save every features result to use it for the feature dataset

--- utils/cache_utils.py
+++ utils/cache_utils.py
@@ -2,8 +2,7 @@ import os
 import pickle 
 
 """
-for saving the results and loading them to save time 
-in the calculation
+for saving the results for every feature of a dataset to save time in the calculations
 """
 
 def save_cache(data,filepath):
@@ -17,3 +16,17 @@ def load_cache(filepath):
         with open(filepath,'rb') as f:
             return pickle.load(f)
     return None
+
+def compute_or_load_feature(feature_name,dataset_name,compute_fn,base_dir='cache',force_recompute=False):
+    path = os.path.join(base_dir,dataset_name,f"{feature_name}.pkl")
+    
+    if not force_recompute:
+        cached = load_cache(path)
+        if cached is not None:
+            print(f"[Cache] loaded: {feature_name}")
+            return cached 
+    
+    print(f"[Compute] Computing : {feature_name}")
+    result = compute_fn()
+    save_cache(result,path)
+    return result
