c7aac14b8a9112bad245a2d18c668f156e9e83f1
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-21 12:32:14 +0300

    Update clean_utils.py
    update it so that it checks if the dataframe has any null values or np.inf before continuing

--- utils/clean_utils.py
+++ utils/clean_utils.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
-#if the features that we extract have some np.nan values or np.inf
+
 def clean_features(df: pd.DataFrame,fill_value: float = 0.0) -> pd.DataFrame:
+
+    has_nan = df.isnull().values.any()
+    has_inf = np.isinf(df.values).any()
+    if not has_nan or not has_inf:
+        return df
+
     df.replace([-np.inf,np.inf],np.nan,inplace=True)
     df.fillna(fill_value,inplace=True)
     return df
-  
