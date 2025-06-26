b714f8fbf4f0af201f6c30e4075c2a65e57dcceb
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-26 14:04:32 +0300

    Update rot.py

--- features/rot.py
+++ features/rot.py
@@ -15,18 +15,18 @@ def compute_rot(df, id_col, time_col, lat_col, lon_col):
     df['time_next'] = df.groupby(id_col)[time_col].shift(-1)
 
     # Heading i
-    df['vx_i'] = (df[lon_col] - df['lon_prev']) / ((df[time_col] - df['time_prev']).dt.total_seconds().replace(0, 1))
-    df['vy_i'] = (df[lat_col] - df['lat_prev']) / ((df[time_col] - df['time_prev']).dt.total_seconds().replace(0, 1))
+    df['vx_i'] = (df[lon_col] - df['lon_prev']) / ((df[time_col] - df['time_prev']).dt.total_seconds()+0.01)
+    df['vy_i'] = (df[lat_col] - df['lat_prev']) / ((df[time_col] - df['time_prev']).dt.total_seconds()+ 0.01)
     heading_i = np.arctan2(df['vy_i'], df['vx_i'])
 
     # Heading i+1
-    df['vx_ip1'] = (df['lon_next'] - df[lon_col]) / ((df['time_next'] - df[time_col]).dt.total_seconds().replace(0, 1))
-    df['vy_ip1'] = (df['lat_next'] - df[lat_col]) / ((df['time_next'] - df[time_col]).dt.total_seconds().replace(0, 1))
+    df['vx_ip1'] = (df['lon_next'] - df[lon_col]) / ((df['time_next'] - df[time_col]).dt.total_seconds()+0.01)
+    df['vy_ip1'] = (df['lat_next'] - df[lat_col]) / ((df['time_next'] - df[time_col]).dt.total_seconds()+0.01)
     heading_ip1 = np.arctan2(df['vy_ip1'], df['vx_ip1'])
 
     # Rate of Turn (ROT)
     df['heading_diff'] = heading_ip1 - heading_i
-    df['rot_time_diff'] = (df['time_next'] - df[time_col]).dt.total_seconds().replace(0, 1)
+    df['rot_time_diff'] = (df['time_next'] - df[time_col]).dt.total_seconds()+0.01
     df['ROT'] = df['heading_diff'] / df['rot_time_diff']
 
     return df.groupby(id_col)['ROT'].agg(
