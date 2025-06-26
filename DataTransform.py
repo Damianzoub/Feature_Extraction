16af0e54daf55f6aae1db11ff491ab887397e7e7
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-26 13:44:49 +0300

    Update DataTransform.py

--- DataTransform.py
+++ DataTransform.py
@@ -3,23 +3,19 @@ import pandas as pd
 from utils.data_loader import load_csv
 from utils.Imputer import transform_dataset
 from utils.cache_utils import save_cache,load_cache
+#connecting the feature_pipeline with DataTransform for the extraction of the features
 from feature_pipeline.feature_pipeline import FeaturePipeline
 class DataTransformer:
-    def __init__(self,dataset_path,time_col='t',id_col='shipid',speed_col='speed',
-                 heading_col='heading',lat_col='lat',lon_col='lon',course_col='course'
-                 ,shiptype_col='shiptype',destination_col='destination',numeric_cols=None,categorical_cols=None):
+    def __init__(self,dataset_path,time_col='t',id_col='shipid',lat_col='lat',lon_col='lon'
+                 ,shiptype_col='shiptype',numeric_cols=None,categorical_cols=None):
         
         self.dataset_path= dataset_path
         self.data = None
         self.time_col=time_col
         self.id_col=id_col
-        self.speed_col=speed_col
-        self.heading_col=heading_col
         self.lat_col=lat_col
         self.lon_col=lon_col
-        self.course_col=course_col
         self.shiptype_col=shiptype_col
-        self.destination_col=destination_col
         self.numeric_cols = numeric_cols
         self.categorical_cols=categorical_cols
 
@@ -27,13 +23,9 @@ class DataTransformer:
             "dataset_path":dataset_path,
             "time_col": time_col,
             "id_col": id_col,
-            "speed_col": speed_col,
-            "heading_col": heading_col,
             "lat_col": lat_col,
             "lon_col": lon_col,
-            "course_col": course_col,
-            "shiptype_col": shiptype_col,
-            "destination_col": destination_col
+            "shiptype_col": shiptype_col     
         }
 
     def load_data(self):
