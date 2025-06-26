744141a52ad898be027782d0e63d186ff2cd4f12
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-26 14:18:02 +0300

    Update acceleration.py

--- features/acceleration.py
+++ features/acceleration.py
@@ -6,7 +6,8 @@ def acceleration_per_id(df, time_col, id_col, speed_col='speed'):
     df = df.sort_values(by=[id_col, time_col])
 
     
-    df['time_diff'] = df.groupby(id_col)[time_col].diff().dt.total_seconds().fillna(1) +0.01 
+    df['time_diff'] = df.groupby(id_col)[time_col].diff().dt.total_seconds().fillna(1) 
+    df['time_diff'] = df['time_diff'].apply(lambda x: x+0.000001 if x==0 else x)
     df['speed_diff'] = df.groupby(id_col)[speed_col].diff().fillna(0)
 
     df['acceleration'] = df['speed_diff'] / df['time_diff']
