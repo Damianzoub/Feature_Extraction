9e546bb7d33794c01ff427d395b2ba1c979d5d91
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-29 19:05:49 +0300

    Update acceleration.py

--- features/acceleration.py
+++ features/acceleration.py
@@ -1,15 +1,18 @@
 from utils.time_utils import categorize_time
 import pandas as pd
+
+
 def acceleration_per_id(df, time_col, id_col, speed_col='speed'):
-    df= categorize_time(df,time_col)
+    df = categorize_time(df, time_col)
     df = df.copy()
     df = df.sort_values(by=[id_col, time_col])
 
-    
-    df['time_diff'] = df.groupby(id_col)[time_col].diff().dt.total_seconds().fillna(1) 
+    # Time and speed differences
+    df['time_diff'] = df.groupby(id_col)[time_col].diff().dt.total_seconds().fillna(1)
     df['time_diff'] = df['time_diff'].apply(lambda x: x+0.000001 if x==0 else x)
     df['speed_diff'] = df.groupby(id_col)[speed_col].diff().fillna(0)
 
+    # Acceleration = Δspeed / Δtime
     df['acceleration'] = df['speed_diff'] / df['time_diff']
 
     return df.groupby(id_col)['acceleration'].agg(
