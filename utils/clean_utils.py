a66edc07c8ae81c67bb8e35c67107570ee9c6257
Damianos Zoumpos <134238559+Damianzoub@users.noreply.github.com>
2025-06-29 19:15:50 +0300

    Update clean_utils.py

--- utils/clean_utils.py
+++ utils/clean_utils.py
@@ -3,12 +3,5 @@ import pandas as pd
 
 def clean_features(df: pd.DataFrame,fill_value: float = 0.0) -> pd.DataFrame:
 
-    numeric_df = df.select_dtypes(include=[np.number])
-    has_nan = df.isnull().values.any()
-    has_inf = np.isinf(numeric_df.values).any()
-    if not has_nan or not has_inf:
-        return df
-
-    df.replace([-np.inf,np.inf],np.nan,inplace=True)
-    df.fillna(fill_value,inplace=True)
+    df.replace([np.inf,-np.inf,np.nan],fill_value,inplace=True)
     return df
